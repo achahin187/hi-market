fe21577ed2a2ec7c26bf1bee7c68e29c2d229260
Ahmed Hamdy <a.hamdy@eramint.net>
2020-11-23 15:50:10 +0200

    upload auth

---
+++ app/Http/Controllers/Api/CartController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Offer;
+use App\Http\Traits\generaltrait;
+class CartController extends Controller
+{
+	use generaltrait;
+
+
+    #function to send promo code
+    #return dicount value
+    public function sendPromoCode(Request $request)
+    {
+    	$request->validate([
+    		'promoCode' => 'required'
+    	]);
+
+    		$getDisount = Offer::Where('promocode', $request->promoCode)->first();
+
+
+    		switch ($getDisount) {
+
+    			case $getDisount->value_type === 'discount by value':
+    				return $this->returnData(['discount', 'type'],[$getDisount->money, $getDisount->value_type ]);
+    				break;
+
+    			case $getDisount->value_type == 'discount by percentage':
+    				return $this->returnData(['discount', 'type'],[$getDisount->money, $getDisount->value_type  ]);
+    				break;
+
+    			case $getDisount->value_type == 'free delivery':
+    				return $this->returnData(['discount', 'type'],[0, $getDisount->value_type  ]);
+    				break;
+    			
+    			default:
+    				return response()->json(['msg'=>'this type not avilable']);
+    				break;
+    		}//end case
+
+    
+
+    }//end function
+}//end class
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -58,7 +58,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('123456789')//'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'team_id' => Team::all()->random()->id,
         'flag' => $faker->randomElement([0,1]),
