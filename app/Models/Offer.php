3ac981a5e764a8c7cf68dd0de33b1134b3523727
Ahmed Hamdy <a.hamdy@eramint.net>
2020-11-23 16:25:11 +0200

    update function  send promo

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -18,6 +18,16 @@ class Offer extends Model
         'arab_name','eng_name','arab_description','eng_description','offer_type','promocode','status','end_date','start_date','value_type','supermarket_id','branch_id','image','created_by','updated_by'
     ];
 
+    public function scopeCheckPromoCode($q, $promoCode)
+    {
+        return $q->Where('promocode', $promoCode);
+    }
+
+    public function scopeCheckSuperMarket($q, $supermarket_id)
+    {
+        return $q->Where('supermarket_id', $supermarket_id);
+    }
+
     public function supermarket() {
         return $this->belongsTo('App\Models\Supermarket');
     }
