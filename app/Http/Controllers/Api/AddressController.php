bc11fb1ef73835899594183f1860a5f48dc792ab
m.osama <m.osama@eramint.com>
2020-11-24 15:58:12 +0200

    update

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -3,25 +3,125 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
+use App\Models\Client;
 use Illuminate\Http\Request;
 use App\Http\Traits\generaltrait;
+use Illuminate\Support\Facades\Validator;
 
 class AddressController extends Controller
 {
     use generaltrait;
 
-    // public function addAddress(Request $request)
-    // {
+     public function updateaddress(Request $request)
+     {
 
-    // 	$request->validate([
-    // 		'lat' => 'required',
-    // 		'lon' => 'required',
-    // 		'location' => 'string|required',
-    // 		'status' => 'required|boolean',
-    // 		'additional' => 'required',
-    // 	]);
-    // }
+         $udid = $request->header('udid');
+
+         $token = $request->header('token');
+
+         $lang = $request->header('lang');
+
+         if(!$lang || $lang == ''){
+
+             return $this->returnError(402,'language is missing');
+         }
+
+         $client = Client::where('remember_token',$token)->select('id','mobile_number','name')->first();
+
+         if($client)
+         {
+             $validator = Validator::make($request->all(), [
+                 'address' => ['required','not_regex:/([%\$#\*<>]+)/'],
+             ]);
+
+             if($validator->fails()) {
+
+                 if ($lang == 'ar') {
+                     return $this->returnError(400, 'بيانات الدخول غير صحيحة');
+                 }
+                 else
+                 {
+                     return $this->returnError(400, 'These data is not valid');
+                 }
+             }
+
+             $address = $request->address;
+             $address_id = $request->address_id;
+
+             Address::where('client_id',$client->id)->where('id',$address_id)->update(['description' => $address]);
+
+             if ($lang == 'ar') {
+                 return $this->returnSuccessMessage('لقد تم تعديل العنوان بنجاح', 200);
+             } else {
+                 return $this->returnSuccessMessage('This address have been updated successfully', 200);
+             }
+         }
+         else
+         {
+             if($lang == 'ar')
+             {
+                 return $this->returnError(305,'لم نجد هذا العميل');
+             }
+             return $this->returnError(305 ,'there is no client found');
+         }
+
+     }
+
+    public function addaddress(Request $request)
+    {
+
+        $udid = $request->header('udid');
+
+        $token = $request->header('token');
+
+        $lang = $request->header('lang');
+
+        if(!$lang || $lang == ''){
+
+            return $this->returnError(402,'language is missing');
+        }
+
+        $client = Client::where('remember_token',$token)->select('id','mobile_number','name')->first();
+
+        if($client)
+        {
+            $validator = Validator::make($request->all(), [
+                'address' => ['required','not_regex:/([%\$#\*<>]+)/'],
+            ]);
+
+            if($validator->fails()) {
+
+                if ($lang == 'ar') {
+                    return $this->returnError(400, 'بيانات الدخول غير صحيحة');
+                }
+                else
+                {
+                    return $this->returnError(400, 'These data is not valid');
+                }
+            }
+
+            $address = $request->address;
+
+            Address::create(['description' => $address]);
+
+            if ($lang == 'ar') {
+                return $this->returnSuccessMessage('لقد تم اضافة العنوان بنجاح', 200);
+            } else {
+                return $this->returnSuccessMessage('This address have been added successfully', 200);
+            }
+        }
+        else
+        {
+            if($lang == 'ar')
+            {
+                return $this->returnError(305,'لم نجد هذا العميل');
+            }
+            return $this->returnError(305 ,'there is no client found');
+        }
+
+    }
 
 
-    
+
 }
