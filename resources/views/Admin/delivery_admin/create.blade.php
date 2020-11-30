c863b274c92319de5143cc71076040ab88e7f8cb
Ahmed Hamdy <a.hamdy@eramint.net>
2020-11-30 14:33:19 +0200

    dash

--- resources/views/Admin/delivery_admin/create.blade.php
+++ resources/views/Admin/delivery_admin/create.blade.php
@@ -56,7 +56,7 @@
 
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">{{__('name')}}</label>
+                                        <label for="exampleInputEmail1">{{__('admin.name')}}</label>
                                         <input type="text" value="@if(isset($admin)){{$admin->name }} @endif" name="name" class=" @error('name') is-invalid @enderror form-control" required>
                                         @error('name')
                                             <span class="invalid-feedback" role="alert">
