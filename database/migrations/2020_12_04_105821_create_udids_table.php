6f9d70c31f05cc35f4c1de2344b0cdb4903b77c0
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-04 13:06:40 +0200

    add udids table

---
+++ database/migrations/2020_12_04_105821_create_udids_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUdidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('udids', function (Blueprint $table) {
+            $table->id();
+            $table->string("body");
+            $table->bigInteger("client_id")->unsigned()->nullable();
+            $table->foreign("client_id")->references("id")->on("clients")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('udids');
+    }
+}
