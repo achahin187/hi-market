e2dbd1105d3afc7c975becc583acaba38cf85cf6
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-04 16:12:23 +0200

    updated favourites

--- app/Http/Controllers/Api/FavouritesController.php
+++ app/Http/Controllers/Api/FavouritesController.php
@@ -22,102 +22,80 @@ class FavouritesController extends Controller
         $product_id = $request->product_id;
         $flag = $request->flag;
         $client = getUser();
-        
-     
+
+
         // flag = 1 add
         //flag = 0 remove
 
-        if($flag == 1) {
+        if ($flag == 1) {
 
-                if (auth("client-api")->check()) {
+            if (auth("client-api")->check()) {
 
-                    
-                    $client_devices = DB::table('client_product')
+
+                $client_devices = DB::table('client_product')
                     ->where('udid', $udid)
                     ->where('client_id', '=', null)
                     ->update(['client_id' => $client->id]);
 
+                $client->products()->attach($product_id, ['udid' => $udid]);
+                return $this->returnSuccessMessage('لقد اصبح هذا المنتج في المفضلات', '');
+
+            } else {
+
+
+                $client_device = DB::table('client_product')
+                    ->where('udid', $udid)
+                    ->where('client_id', '!=', null)
+                    ->first();
+
+                if ($client_device) {
+
+
+                    $client = Client::find($client_device->client_id);
+
                     $client->products()->attach($product_id, ['udid' => $udid]);
-                     return $this->returnSuccessMessage('لقد اصبح هذا المنتج في المفضلات', '');
+
+                    return $this->returnSuccessMessage('لقد اصبح هذا المنتج في المفضلات', '');
 
                 } else {
 
+                    $device = DB::table('client_product')->insert(['udid' => $udid, 'product_id' => $product_id]);
 
-                        $client_device = DB::table('client_product')
-                        ->where('udid', $udid)
-                        ->where('client_id', '!=', null)
-                        ->first();
 
-                        if ($client_device) {
-                       
-
-                            $client = Client::find($client_device->client_id);
-
-                            $client->products()->attach($product_id, ['udid' => $udid]);
-
-                                return $this->returnSuccessMessage('لقد اصبح هذا المنتج في المفضلات', '');
-                          
-                        } else {
-
-                            $device = DB::table('client_product')->insert(['udid' => $udid, 'product_id' => $product_id]);
-
-                           
-                                return $this->returnSuccessMessage('لقد اصبح هذا المنتج في المفضلات', '');
-                          
-                        }
+                    return $this->returnSuccessMessage('لقد اصبح هذا المنتج في المفضلات', '');
 
                 }
-        }
-        else
-        {
-            DB::table('client_product')->where('udid',$udid)->where('product_id',$product_id)->delete();
+
+            }
+        } else {
+            DB::table('client_product')->where('udid', $udid)->where('product_id', $product_id)->delete();
 
             return $this->returnSuccessMessage('تم الغاء الاعجاب', '');
- 
+
         }
 
     }
 
     public function getfavourites(Request $request)
     {
-        //
 
-        $udid = $request->header('udid');
-
-        $token = $request->header('token');
-
-        $lang = $request->header('lang');
-
-        if(!$lang || $lang == ''){
-
-            if ($lang == 'ar') {
-                return $this->returnError(402,'اللغة غير موجودة');
-            }
-            else
-            {
-                return $this->returnError(402,'language is missing');
-            }
-        }
-
-        $product_id = $request->product_id;
-
-        $client = Client::where('remember_token',$token)->first();
+        $client = getUser();
 
 
         if ($client) {
 
-            $favproducts = $client->products()->where('udid',$udid)->select()->get();
+            $favproducts = $client->products()->where( function ($query) {
+                if($udid  =\request()->header("udid"))
+                {
+                    $query->where("udid",$udid);
+                };
+            })->select()->get();
 
             return $this->returnData(['favourite products'], [$favproducts]);
 
-        }
-        else
-        {
-            if($lang == 'ar')
-            {
-                return $this->returnError(400,'لم نجد هذا العميل');
-            }
-            return $this->returnError(400,'no client exists');
+        } else {
+
+            return $this->returnError(404, 'no client exists');
         }
 
     }
--- routes/api.php
+++ routes/api.php
@@ -1,81 +1,80 @@
 <?php
 
-    use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-    /*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | is assigned the "api" middleware group. Enjoy building your API!
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-        Route::group(['namespace' => 'Api'],function () {
+Route::group(['namespace' => 'Api'], function () {
 
-            Route::post('register', 'AuthController@register')->name('client.register');
-            Route::post('login', 'AuthController@login')->name('client.login');
-            Route::post('forgetpassword','AuthController@forgetpassword')->name('forgetpassword');
-        Route::post('verifycode', 'AuthController@verifycode')->name('profile_update');
-        Route::post('resetpassword', 'ClientController@resetpassword')->name('reset_password');
-        Route::post('changepassword', 'ClientController@changepassword')->name('change_password');
-            Route::get('products/search/{name}','ProductController@getproductsearch')->name('search');
-            Route::get("products/filter","ProductController@filter");
-          });
+    Route::post('register', 'AuthController@register')->name('client.register');
+    Route::post('login', 'AuthController@login')->name('client.login');
+    Route::post('forgetpassword', 'AuthController@forgetpassword')->name('forgetpassword');
+    Route::post('verifycode', 'AuthController@verifycode')->name('profile_update');
+    Route::post('resetpassword', 'ClientController@resetpassword')->name('reset_password');
+    Route::post('changepassword', 'ClientController@changepassword')->name('change_password');
+    Route::get('products/search/{name}', 'ProductController@getproductsearch')->name('search');
+    Route::get("products/filter", "ProductController@filter");
+});
 
-    Route::group(['namespace' => 'Api','middleware'=>'check_mobile_serial'],function () {
+Route::group(['namespace' => 'Api', 'middleware' => 'check_mobile_serial'], function () {
 
-        Route::get('logout', 'AuthController@logout')->name('logout');
+    Route::get('logout', 'AuthController@logout')->name('logout');
 
-        Route::get('products','ProductController@index')->name('listproducts');
-        Route::post('product','ProductController@productdetails')->name('productdetails');
+    Route::get('products', 'ProductController@index')->name('listproducts');
+    Route::post('product', 'ProductController@productdetails')->name('productdetails');
 
-        Route::get('categories','CategoriesController@index');
-        Route::get('user', 'AuthController@getAuthUser')->name('client.auth');
-        Route::get('social/{flag}', 'AuthController@social')->name('social.auth');
+    Route::get('categories', 'CategoriesController@index');
+    Route::get('user', 'AuthController@getAuthUser')->name('client.auth');
+    Route::get('social/{flag}', 'AuthController@social')->name('social.auth');
 
-        Route::post('profile','ClientController@client_profile')->name('client_profile');
-        Route::post('profile/update', 'ClientController@updateprofile')->name('profile_update');
-        //address
-        Route::post('add_address','ClientController@add_address')->name('address.store');
-        Route::get('get_address/{flag}', 'ClientController@get_address')->name('address.show');
-
-
-
-        Route::post('favourites/add','FavouritesController@addfavourite');
-        Route::post('favourites/remove','FavouritesController@removefavourites');
-        Route::get('clientorders','OrderController@clientorders')->name('client_orders');
-        Route::get('order/{order_id}','OrderController@getorder')->name('order_details');
-        Route::post('cart','OrderController@addcart')->name('order_details');
+    Route::post('profile', 'ClientController@client_profile')->name('client_profile');
+    Route::post('profile/update', 'ClientController@updateprofile')->name('profile_update');
+    //address
+    Route::post('add_address', 'ClientController@add_address')->name('address.store');
+    Route::get('get_address/{flag}', 'ClientController@get_address')->name('address.show');
 
 
-        Route::post('getarea','LocationController@index')->name('listArea');
-        Route::get('gethomedata','ProductController@homeData')->name('listHome');
-        Route::post('getsupermarketcats','CategoriesController@supermarketcategories')->name('listsupermarketcats');
-        Route::post('getcategoryproducts','CategoriesController@categoryproducts')->name('listcategoryproducts');
-        Route::post('supermarketoffers','CategoriesController@supermarketoffers')->name('listcategoryproducts');
+    Route::post('favourites/add', 'FavouritesController@addfavourite');
+    Route::post('favourites/remove', 'FavouritesController@removefavourites');
+    Route::get('clientorders', 'OrderController@clientorders')->name('client_orders');
+    Route::get('order/{order_id}', 'OrderController@getorder')->name('order_details');
+    Route::post('cart', 'OrderController@addcart')->name('order_details');
+    Route::get("fav/get", "FavouritesController@getfavourites");
 
-        Route::post('clientpoints','ClientController@clientpoints')->name('listcategoryproducts');
-        Route::post('clientaddresses','ClientController@clientaddresses')->name('listcategoryproducts');
+    Route::post('getarea', 'LocationController@index')->name('listArea');
+    Route::get('gethomedata', 'ProductController@homeData')->name('listHome');
+    Route::post('getsupermarketcats', 'CategoriesController@supermarketcategories')->name('listsupermarketcats');
+    Route::post('getcategoryproducts', 'CategoriesController@categoryproducts')->name('listcategoryproducts');
+    Route::post('supermarketoffers', 'CategoriesController@supermarketoffers')->name('listcategoryproducts');
+
+    Route::post('clientpoints', 'ClientController@clientpoints')->name('listcategoryproducts');
+    Route::post('clientaddresses', 'ClientController@clientaddresses')->name('listcategoryproducts');
 
 
-        //sendPromoCode
-        Route::post('sendPromoCode','CartController@sendPromoCode');
+    //sendPromoCode
+    Route::post('sendPromoCode', 'CartController@sendPromoCode');
 
-        Route::post('updateaddress','ClientController@update_address');
+    Route::post('updateaddress', 'ClientController@update_address');
 
-    });
+});
 
 
-     Route::group(['namespace' => 'Api'],function () {
-        //address
-        Route::post('deleteAddress','ClientController@delete_address');
+Route::group(['namespace' => 'Api'], function () {
+    //address
+    Route::post('deleteAddress', 'ClientController@delete_address');
 
-    });
+});
 
 
 
