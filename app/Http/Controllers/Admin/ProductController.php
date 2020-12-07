4d13f316b8bc8503b549355a5c5b39b26d15ff8f
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-07 13:46:22 +0200

    fawarnaha

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends Controller
      */
     public function index($flag)
     {
-        dd($flag);
+        dd(request()->flag);
         if($flag == 1)
         {
             $products = Product::where('flag',$flag)->orderBy('id', 'desc')->get();
