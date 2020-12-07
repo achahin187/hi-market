f3dc1e7109fc2bac80406aa67d75def8a30e1eb3
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-07 10:58:40 +0200

    added notification

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\OfferResource;
+use App\Http\Resources\OrderResource;
 use App\Http\Traits\GeneralTrait;
 use App\Models\Offer;
 use Illuminate\Http\Request;
@@ -13,15 +15,22 @@ class NotificationController extends Controller
 
     public function __construct()
     {
-        if (\request("Authorization")) {
+        if (\request()->header("Authorization")) {
             $this->middleware("auth:client-api");
         }
     }
 
     public function index()
     {
-        $orders = getUser()->orders;
+        if($user = getUser())
+        {
+        $orders = $user->orders;
         $offers = Offer::all();
-        return $this->returnData(["offers", "orders"], [$offers, $orders]);
+        return $this->returnData(["offers", "orders"], [OfferResource::collection($offers),OrderResource::collection( $orders)]);
+
+        }else{
+            return $this->returnError(422,"User No Exists");
+        }
+
     }
 }
