519d78b200bba70e74af655a7aaf14b1d0be6f61
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-07 16:44:40 +0200

    fixed a bug

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -10,31 +10,31 @@ class ProductResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  Request  $request
+     * @param Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            "id"=>$this->id,
-            "name"=>$this->name,
-            "description"=>$this->description,
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
             "overview" => $this->specs,
-            "price"=>$this->price ?? 0,
-            "offer_price"=>$this->offer_price ?? 0,
-            "rate"=>$this->rate ?? 0,
-             "rating"=>$this->rating ??0,
-            "priority"=>$this->priority ?? 0,
-            "images"=>asset("images/".$this->images) ,
-            "points"=>$this->points ?? 0,
-            "category_name"=>$this->category->name ?? "",
-            "category_id"=>$this->category_id ?? "",
-            "flag"=>$this->flag ?? 0,
-            "supermarket_id"=>$this->supermarket_id ?? 0,
-            "favourite"=>$this->favourite ?? 0,
-            "percentage"=>$this->percentage ?? 0,
-            "imagepath"=>$this->imagepath ?? "default.png",
-            "category"=>$this->category ?? "",
+            "price" => $this->price ?? 0,
+            "offer_price" => $this->offer_price ?? 0,
+            "rate" => $this->rate ?? 0,
+            "rating" => (string) $this->rating ?? "0",
+            "priority" => $this->priority ?? 0,
+            "images" => asset("images/" . $this->images),
+            "points" => $this->points ?? 0,
+            "category_name" => $this->category->name ?? "",
+            "category_id" => $this->category_id ?? "",
+            "flag" => $this->flag ?? 0,
+            "supermarket_id" => $this->supermarket_id ?? 0,
+            "favourite" => $this->favourite ?? 0,
+            "percentage" => $this->percentage ?? 0,
+            "imagepath" => $this->imagepath ?? "default.png",
+            "category" => $this->category ?? "",
 
 
         ];
