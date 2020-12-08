390ca8238c6784ba84e643bdcb8687f2edf60db9
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-08 21:33:08 +0200

    change created at to be ordered

--- app/Pipeline/CreatedAt.php
+++ app/Pipeline/CreatedAt.php
@@ -6,9 +6,14 @@ namespace App\Pipeline;
 
 class CreatedAt extends Filter
 {
+    protected function canSkipRequest()
+    {
+        return request($this->filterRequest()) && request($this->filterRequest()) == false;
+    }
+
     protected function query($builder)
     {
 
-        return $builder->whereDate("created_at",date("Y-m-d",strtotime(request($this->filterRequest()))));
+        return $builder->orderBy("created_at",request("order","asc"));
     }
 }
