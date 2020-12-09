e47f8b8985a0f5a1118341e05b32816d54d3de1e
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-09 12:39:06 +0200

    fixed a bug

--- app/Http/Resources/WishlistResource.php
+++ app/Http/Resources/WishlistResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Branch;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class WishlistResource extends JsonResource
@@ -9,21 +10,28 @@ class WishlistResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            "id"=>$this->id,
-            "name"=>$this->name,
-            "price"=>$this->price??0,
-            "offer_price"=>$this->offer_price ?? 0,
-            "percentage"=>($this->offer_price / $this->price) * 100,
-            "imagepath"=>asset($this->image),
-            "categoryname"=>$this->category->name,
-            "supermarketname"=>$this->supermarket->name,
-            "description" =>$this->description
+            "id" => $this->id,
+            "name" => $this->name,
+            "price" => $this->price ?? 0,
+            "offer_price" => $this->offer_price ?? 0,
+            "percentage" => ($this->offer_price / $this->price) * 100,
+            "imagepath" => asset($this->image),
+            "category_id" => $this->category->id,
+
+            "category_name" => $this->category->name,
+
+            "supermarket_id" => Branch::find(request("supermarket_id"))->id,
+
+            "supermarket_name" => Branch::find(request("supermarket_id"))->name,
+
+
+            "description" => $this->description
 
         ];
     }
