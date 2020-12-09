7fd3e0cd398aabf97bc35f60404e10e99061ed6c
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-09 14:45:54 +0200

    fixed a bug

--- helper/helpers.php
+++ helper/helpers.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Models\Client;
+use App\Models\Udid;
 
 if (!function_exists("getUser")) {
     function getUser()
     {
 
-            return auth('client-api')->check() ? auth('client-api')->user() : Client::where('unique_id', request()->header('udid'))->first();
+
+            return auth('client-api')->check() ? auth('client-api')->user() : Udid::where("body",request()->header("udid"))->first();
 
 
     }
