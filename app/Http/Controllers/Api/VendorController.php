354fcb8cf8a15a3af34b77a53939ae51ad308183
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-09 22:57:02 +0200

    fixed a bug

--- app/Http/Controllers/Api/VendorController.php
+++ app/Http/Controllers/Api/VendorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\VendorResource;
 use App\Http\Traits\GeneralTrait;
+use App\Models\Product;
 use App\Models\Vendor;
 
 
@@ -14,10 +15,21 @@ class VendorController extends Controller
 
     public function show($id)
     {
+        $validation = \Validator::make(request()->all(), [
+            "supermarket_id" => "required|exists:branches,id",
+            "category_id" => "required|exists:categories,id"
+
+        ]);
+        if ($validation->fails()) {
+            return $this->returnValidationError(422, $validation);
+        }
         $vendors = Vendor::whereHas("categories", function ($query) use ($id) {
             $query->where("categories.id", $id);
         })->get();
-        return $this->returnData(["vendors"], [VendorResource::collection($vendors)]);
+        $product_count = Product::where("branches", function ($query) {
+            $query->where("branches.id", request("supermarket_id"));
+        })->where("category_id", request("category_id"))->filter()->count();
+        return $this->returnData(["vendors", "product_count"], [VendorResource::collection($vendors), $product_count]);
     }
 
 }
