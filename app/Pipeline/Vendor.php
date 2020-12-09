8fc2f8beb7daf443852bdb385a79e9f6bceeccd9
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-09 19:52:15 +0200

    updated filter

--- app/Pipeline/Vendor.php
+++ app/Pipeline/Vendor.php
@@ -8,11 +8,14 @@ class Vendor extends Filter
 {
     protected function canRunRequest()
     {
-        return request( "vendor") ;
+        return request("vendor");
     }
 
     protected function query($builder)
     {
-        return $builder->whereIn("vendor_id",explode( ",",request("vendor")));
+        return $builder->whereHas("vendors", function ($query)
+        {
+            $query->where("vendors.id",explode(",",request("vendor")));
+        });
     }
 }
