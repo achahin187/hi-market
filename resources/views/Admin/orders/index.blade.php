37b8e1efe96bd114ca070fc68b47945517f212f5
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-13 10:56:14 +0200

    add changes

--- resources/views/Admin/orders/index.blade.php
+++ resources/views/Admin/orders/index.blade.php
@@ -185,7 +185,7 @@
                                                 @endif
 
                                                 <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'previous','order_id'=>$order->id]) }}" class="btn btn-success {{ $order->status == 0 ? 'disabled' : '' }}">Previous</a></td>
-                                                <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'next','order_id'=>$order->id]) }}" class="btn btn-primary {{ $order->status == count($status) ? 'disabled' : '' }}" >Next</a></td>
+                                                <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'next','order_id'=>$order->id]) }}" class="btn btn-primary {{ $order->status == 5 ? 'disabled' : '' }}" >Next</a></td>
                                                 <td></td>
                                                 <td>
 
