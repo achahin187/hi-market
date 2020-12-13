e3790a88d35ec0a875323f0839119e176a4b1c02
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-13 20:00:48 +0200

    fixed a bug

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -17,14 +17,7 @@ class OrderResource extends JsonResource
         return [
             "id"=>$this->id,
 
-            "address"=>$this->address,
-            "rate"=>$this->rate ?? 0,
-            "delivery_date"=>$this->delivery_date ?? "",
-            "delivery_rate"=>$this->delivery_rate ?? "",
-            "client_review"=>$this->client_review?? "",
-            "review_status"=>$this->review_status?? "",
-            "order_price"=>$this->order_price?? "",
-            "status"=>$this->status,
+           "message"=>"Your Order Was "
 
         ];
     }
