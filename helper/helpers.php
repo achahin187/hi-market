7d39d889266143445d738f4e850e4873a73d6dce
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-13 19:28:34 +0200

    fixed a bug

--- helper/helpers.php
+++ helper/helpers.php
@@ -8,7 +8,7 @@ if (!function_exists("getUser")) {
     function getUser()
     {
 
-            return auth('client-api')->check() ? auth('client-api')->user() : Client::where("body",request()->header("udid"))->first();
+            return auth('client-api')->check() ? auth('client-api')->user() : Client::where("unique_id",request()->header("udid"))->first();
 
 
     }
