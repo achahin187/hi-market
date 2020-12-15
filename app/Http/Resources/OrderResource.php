f9624c22022cff0795c186aef993f16019f0fe9e
Mostafa Hesham <motaheshamabdel@gmail.com>
2020-12-15 11:14:59 +0200

    updated and added Constants.php

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,23 +2,32 @@
 
 namespace App\Http\Resources;
 
+use App\Constants;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
 {
+    private $messages = [
+        Constants::ORDER_NEW => "New Order Created",
+        Constants::ORDER_APPROVED => "You Approved New Order",
+        Constants::ORDER_DELIVERED => "Your Order Was Delivered Rate Your Order",
+        Constants::ORDER_RECEIVED => "You Received New Order",
+        null => ""
+    ];
+
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            "id"=>$this->id,
+            "id" => $this->id,
 
-           "message"=>"Your Order Was "
-
+            "message" => $this->messages[$this->status],
+            "status" => $this->status,
         ];
     }
 }
