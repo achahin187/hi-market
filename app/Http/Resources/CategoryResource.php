ad36a6ae8fc485c7b209823e581e1b4c05e55509
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-20 14:20:14 +0200

    update image point

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,7 +18,7 @@ class CategoryResource extends JsonResource
             "id"=>$this->id,
             "name"=>$this->name,
             "image"=>$this->image,
-            "imagepath"=>$this->imagepath,
+            "imagepath"=>asset('category_images/'.$this->image),
 
         ];
     }
