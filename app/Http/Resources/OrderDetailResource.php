8ebb1e8073208e79c5832d5c31200dd192ca2d05
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-21 00:08:47 +0200

    order detais

--- app/Http/Resources/OrderDetailResource.php
+++ app/Http/Resources/OrderDetailResource.php
@@ -20,9 +20,11 @@ class OrderDetailResource extends JsonResource
             'status' => $this->getStatus(),
             'date' => Carbon::parse($this->delvery_date)->format('M d Y'),
             
+            'address'=>[
                 'name' => $this->client->name,
-                'desc' => $this->client->addresses->first()->name,
+                'desc' => $this->client->addresses->first()->description,
                 'phone' => $this->client->addresses->first()->phone,
+            ],
 
            
             
