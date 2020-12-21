002471bab4218714a4b0a796f73119fd6504117b
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-21 05:32:15 +0200

    check location

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Polygons\PointLocation;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -278,6 +278,30 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['lo
         //     $count = collect($rate)->avg();
         //     dd( $count );
         // });
+
+
+        Route::get('check/location',function(){
+
+            $pointLocation = new pointLocation();
+            $points = array("50 70","70 40","-20 30","100 10","-10 -10","40 -20","110 -20");
+            $polygon = array("-50 30","50 70","100 50","80 10","110 -10","110 -30","-20 -50","-30 -40","10 -10","-10 10","-30 -20","-50 30");
+            // The last point's coordinates must be the same as the first one's, to "close the loop"
+            foreach($points as $key => $point) {
+                echo "point " . ($key+1) . " ($point): " . $pointLocation->pointInPolygon($point, $polygon) . "<br>";
+            }
+
+            // Results 
+            /*
+            This will output:
+            point 1 (50 70): vertex
+            point 2 (70 40): inside
+            point 3 (-20 30): inside
+            point 4 (100 10): outside
+            point 5 (-10 -10): outside
+            point 6 (40 -20): inside
+            point 7 (110 -20): boundary
+
+        });
 
     });
 
