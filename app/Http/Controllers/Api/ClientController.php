4eaaa71f637b78aa9ae63ea56b5ebb58475998de
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-22 11:20:53 +0200

    asd

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -266,6 +266,7 @@ class ClientController extends Controller
 
        $client_address =  Auth('client-api')->user()->addresses->where('default',1)->first();
        $check =  Address::Where('id', $request->address_id)->where('client_id', Auth('client-api')->user()->id )->get();
+       return $check;
        if ($check) {
            
 
