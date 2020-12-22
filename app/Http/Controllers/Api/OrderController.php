f31cf2405c133393baca1653dfd9738a30964d6a
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-22 11:40:22 +0200

    asd

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -93,7 +93,7 @@ class OrderController extends Controller
         if ($client) {
 
             if (request("asap") == 1) {
-                $order_details["delivery_date"] = Carbon\Carbon::now();
+                $order_details["delivery_date"] = Carbon::now();
             } else {
 
                 $order_details["delivery_date"] = str_replace("/","-",date('Y/m/d', strtotime($request->day)) ).' ' .date('H:i', strtotime($request->time));
