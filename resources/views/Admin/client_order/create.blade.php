39faab251f6e4a83ce43bf176ad56cc63dd820a2
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-23 23:21:31 +0200

    update  wishlist

--- resources/views/Admin/client_order/create.blade.php
+++ resources/views/Admin/client_order/create.blade.php
@@ -126,7 +126,7 @@
                     <select name="supermarket_id" 
                    {{--  @if(session()->get('supermarket_id') != $supermarket->id) class="  
                      form-control select2 click_here" @else class="  
-                     form-control select2"@endif  --}} >
+                     form-control select2"@endif  --}} form-control select2>
                  
                 <option  selected  disabled>Please Select Source</option>
 
