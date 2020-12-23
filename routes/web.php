105bfd5bbe2464677c70c27a6a73fad11151b594
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-24 00:00:46 +0200

    update  wishlist

--- routes/web.php
+++ routes/web.php
@@ -279,16 +279,16 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['lo
         //     dd( $count );
         // });
 
-
-        Route::get('check/location',function(){
+// "70 40","-20 30","100 10","-10 -10","40 -20","110 -20"
+        Route::get('check/location/test',function(){
 
             $pointLocation = new pointLocation();
-            $points = array("50 70","70 40","-20 30","100 10","-10 -10","40 -20","110 -20");
+            $points = array("50 70");
             $polygon = array("-50 30","50 70","100 50","80 10","110 -10","110 -30","-20 -50","-30 -40","10 -10","-10 10","-30 -20","-50 30");
             // The last point's coordinates must be the same as the first one's, to "close the loop"
-            foreach($points as $key => $point) {
-                echo "point " . ($key+1) . " ($point): " . $pointLocation->pointInPolygon($point, $polygon) . "<br>";
-            }
+                echo "point ahmed" . ($key+1) . " ($point): " . $pointLocation->pointInPolygon($points, $polygon) . "<br>";
+            // foreach($points as $key => $point) {
+            // }
 
             // Results 
           
