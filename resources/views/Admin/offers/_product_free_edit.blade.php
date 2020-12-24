ad156de0371d0591d5ad258abf4e01f2a17097fc
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-25 00:47:30 +0200

    add

--- resources/views/Admin/offers/_product_free_edit.blade.php
+++ resources/views/Admin/offers/_product_free_edit.blade.php
@@ -11,7 +11,7 @@
                             <option  selected  disabled>Please Select branch</option>
                             
                                @foreach($branches  as $branch)
-                                 <option value="{{$branch->id}}" {{ in_array($branch->id, $offer->branches->pluck('id'))?'selected' :'' }}>{{ $branch->name }}</option>
+                                 <option value="{{$branch->id}}" {{ in_array($branch->id, $offer->branches->pluck('id')->toArray())?'selected' :'' }}>{{ $branch->name }}</option>
                               @endforeach     
                         </select>
                     </div>
