5899d049e6c8c139f2b59fb8cae8577fc69f20b3
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-25 00:45:48 +0200

    add

--- resources/views/Admin/offers/_promocode_edit.blade.php
+++ resources/views/Admin/offers/_promocode_edit.blade.php
@@ -39,7 +39,7 @@
 
                             
                                @foreach($branches  as $branch)
-                                 <option value="{{$branch->id}}" {{ in_array($branch->id, $offer->branches->pluck('id'))?'selected' :'' }}>{{ $branch->name }}</option>
+                                 <option value="{{$branch->id}}" {{ in_array($branch->id, $offer->branches->pluck('id')->toArray())?'selected' :'' }}>{{ $branch->name }}</option>
                               @endforeach    
                         </select>
                     </div>
