083b37aab53b9ecc233972eeec4cd5eb9e2eb026
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-24 10:10:54 +0200

    update validation phone

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -333,7 +333,7 @@ class ClientController extends Controller
             'default' => ['boolean'],
             'lat' => ['required', 'string'],
             'lon' => ['required', 'string'],
-            'phone' => ['required', 'unique:addresses,phone'],
+            'phone' => 'required|unique:addresses,phone',
             'notes' => ['nullable'],
             'govern' => 'required|string',
             'name' => 'required|string',
