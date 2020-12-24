a6fa15453fc170d2c3d978c18e4f0a2ae8625c81
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-24 16:40:50 +0200

    update points

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -137,7 +137,7 @@ class OrderController extends Controller
                     "product_id" => explode(":", $product)[0],
                     "quantity"   => explode(":", $product)[1],
                     "price"      => explode(":", $product)[2],
-                    "point" => Product::Where('id', explode(":", $product)[0] )->first()->points * explode(":", $product)[1],
+                    "points" => Product::Where('id', explode(":", $product)[0] )->first()->points * explode(":", $product)[1],
                 ]);
 
              }   
