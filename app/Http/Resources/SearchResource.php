886675f4a3854b3bb80d2a7c87ddc319f73fc7fb
$   git config --global user.name Your <Ahmedhamdy-eng>
2020-12-24 21:52:48 +0200

    fuaj

--- app/Http/Resources/SearchResource.php
+++ app/Http/Resources/SearchResource.php
@@ -20,29 +20,40 @@ class SearchResource extends JsonResource
 
             "name" => $this->getProduct()->name,
             "description" => $this->getProduct()->description,
-            "overview" => $this->getProduct()->specs ?? "",
+            //"overview" => $this->getProduct()->specs ?? "",
             "price" => $this->getProduct()->price ?? 0,
-            "offer_price" => $this->getProduct()->offer_price ?? 0,
+            //"offer_price" => $this->getProduct()->offer_price ?? 0,
             "rate" => $this->getProduct()->rate ?? 0,
-            "ratings" => $this->getProduct()->ratings ?? "0",
-            "priority" => $this->getProduct()->priority ?? 0,
+            //"ratings" => $this->getProduct()->ratings ?? "0",
+            //"priority" => $this->getProduct()->priority ?? 0,
             "images" => asset("images/" . $this->getProduct()->images),
             "points" => $this->getProduct()->points ?? 0,
-            "category_name" => $this->getProduct()->category->name ?? "",
-            "category_id" => $this->getProduct()->category_id ?? "",
-            "flag" => $this->getProduct()->flag ?? 0,
+            "category" => $this->getProduct()->category->name ?? "",
+            //"category_id" => $this->getProduct()->category_id ?? "",
+            //"flag" => $this->getProduct()->flag ?? 0,
             "supermarketId" => $this->getBranch()->id,
-            "supermarketName" =>$this->getBranch()->name,
-            "favourite" => (int)(\DB::table("client_product")->where("product_id",$this->getProduct()->id)->where("udid",request()->header("udid"))->count() != 0),
-            "percentage" => $this->getProduct()->price && $this->getProduct()->offer_price ? (100 - (($this->getProduct()->offer_price / $this->getProduct()->price) * 100)) : 0,
-            "imagepath" => $this->getProduct()->imagepath ?? "default.png",
-            "category" => $this->getProduct()->category ?? "",
-            "quantity" => (int)$this->getProduct()->qty,
+            //"supermarketName" =>$this->getBranch()->name,
+            //"favourite" => (int)(\DB::table("client_product")->where("product_id",$this->getProduct()->id)->where("udid",request()->header("udid"))->count() != 0),
+            //"percentage" => $this->getProduct()->price && $this->getProduct()->offer_price ? (100 - (($this->getProduct()->offer_price / $this->getProduct()->price) * 100)) : 0,
+            //"imagepath" => $this->getProduct()->imagepath ?? "default.png",
+            //"category" => $this->getProduct()->category ?? "",
+            //"quantity" => (int)$this->getProduct()->qty,
             'vendor' => $this->getProduct()->name ?? "",
         ];
     }
 
-
+//'id' => $product->id,
+                //                 'name' => $product->name_ar,
+                //                 'description' => $product->arab_description,
+                //                 'rate' => $product->rate,
+                //                 'supermarketId'=> $product->branch_id,
+                //                 'price' => $product->price,
+                //                 'offer_price' => $product->offer_price,
+                //                 'images' => asset('product_images/'.$product->images),
+                //                 'points' => $product->points,
+                //                 'category' => !is_null($product->category) ? $product->category->name_ar : "",
+                //                 'vendor' => !is_null($product->vendor) ? $product->vendor->arab_name : ""
+                //             ];
            
 
     private function getProduct()
