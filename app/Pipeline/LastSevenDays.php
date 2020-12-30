e41010f8db4a3d6c8f1245e442918e23a657d656
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-30 10:18:50 +0200

    fdsgsd

--- app/Pipeline/LastSevenDays.php
+++ app/Pipeline/LastSevenDays.php
@@ -8,12 +8,12 @@ class LastSevenDays extends Filter
 {
     protected function canRunRequest()
     {
-        return request(  "new_arrival") && request("new_arrival") == 1;
+        return request("new_arrival") && request("new_arrival") == 1;
     }
 
     protected function query($builder)
     {
 
-        return $builder->whereDate("created_at",">",date("Y-m-d H:i:s",now()->subDays(7)->timestamp));
+        return $builder->whereDate("category_supermarket.created_at",">",date("Y-m-d H:i:s",now()->subDays(7)->timestamp));
     }
 }
