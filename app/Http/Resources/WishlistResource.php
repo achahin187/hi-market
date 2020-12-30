7e38282bf92914568fbbd689286e1c700a530b92
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-30 22:43:26 +0200

    asdfd

--- app/Http/Resources/WishlistResource.php
+++ app/Http/Resources/WishlistResource.php
@@ -23,7 +23,7 @@ class WishlistResource extends JsonResource
             "price" => $this->price ?? 0,
             "offer_price" => $this->offer_price ?? 0,
             "percentage" => ($this->offer_price / $this->price) * 100,
-            "imagepath" => asset($this->image),
+            "imagepath" => asset("product_images/" . $this->images),
             "flag"=>$this->flag,
             "images" => asset("product_images/" . $this->images),
             "supermarket_id" => Branch::find(request("supermarket_id"))->id,
