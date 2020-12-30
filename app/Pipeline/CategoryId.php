3e7dcfa2f5bdebc44e5f05bb0d286636f0b2f466
Ahmed Hamdy <a.hamdy@eramint.net>
2020-12-30 10:40:42 +0200

    af

--- app/Pipeline/CategoryId.php
+++ app/Pipeline/CategoryId.php
@@ -6,10 +6,15 @@ namespace App\Pipeline;
 
 class CategoryId extends Filter
 {
+	protected function canRunRequest()
+	{
+		return request($this->filterRequest());
+	}
+
     protected function query($builder)
     {
         return $builder->whereHas("category", function ($query) {
-            $query->whereIn("id",explode(",", request($this->filterRequest())));
+            $query->where("id",request($this-filterRequest()));
         });
     }
 }
