ebf372dceab902a342f5a33dc2f805f8d7340377
$   git config --global user.name Your <Ahmedhamdy-eng>
2021-01-01 20:47:25 +0200

    update route api search-home and make notification table and update rate in view product

--- database/migrations/2021_01_01_201222_create_notifications_table.php
+++ database/migrations/2021_01_01_201222_create_notifications_table.php
@@ -14,14 +14,11 @@ class CreateNotificationsTable extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->string('arab_title');
-            $table->string('eng_title');
-            $table->string('arab_body');
-            $table->string('eng_body');
-            $table->string('icon');
-            $table->unsignedInteger('flag');
-            $table->unsignedInteger('model_id')->nullable();
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
