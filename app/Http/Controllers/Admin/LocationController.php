0fc1a2d9979b6ee8eace965d967d21a08aa1dedc
Mostafa Hesham <motaheshamabdel@gmail.com>
2021-01-01 21:36:18 +0200

    fixed a bug add location classes the favorite in similar products was always return zero

--- app/Http/Controllers/Admin/LocationController.php
+++ app/Http/Controllers/Admin/LocationController.php
@@ -3,75 +3,77 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Location\Geometry\Bounds;
+use App\Location\Geometry\Point;
 use Illuminate\Http\Request;
 use App\Models\Location;
 use App\Models\City;
 use App\Models\Area;
 use App\Models\Polygon;
 use App\Polygons\PointLocation;
-    
+
 class LocationController extends Controller
-{	
-	public $blade;
-	public $route;
+{
+    public $blade;
+    public $route;
 
-	public function __construct()
-	{
-		$this->blade = 'Admin.locations.' ;
-        $this->route = 'locations.' ;
-	}
+    public function __construct()
+    {
+        $this->blade = 'Admin.locations.';
+        $this->route = 'locations.';
+    }
 
-	public function index()
-	{
+    public function index()
+    {
 
-		$locations = City::withCount('areaList')->orderBy('area_list_count', 'desc')->get(); 
-        
-		return view($this->blade.__FUNCTION__)->with('locations',$locations);
-	}
+        $locations = City::withCount('areaList')->orderBy('area_list_count', 'desc')->get();
 
-	public function getArea($id)
-	{
-		$areaLists = City::find($id)->areaList()->get();
-	
-		return view($this->blade.'area')->with('areaLists',$areaLists);
-	}
+        return view($this->blade . __FUNCTION__)->with('locations', $locations);
+    }
+
+    public function getArea($id)
+    {
+        $areaLists = City::find($id)->areaList()->get();
+
+        return view($this->blade . 'area')->with('areaLists', $areaLists);
+    }
 
 
-	public function create()
-	{	
-		$cities = City::all();
-		return view($this->blade.__FUNCTION__)->with('cities',$cities);
-	}
+    public function create()
+    {
+        $cities = City::all();
+        return view($this->blade . __FUNCTION__)->with('cities', $cities);
+    }
 
 
     public function addLocation(request $request)
     {
-    	 $locations =  collect (json_decode($request->data, true)) ;
-    	 	
-    	 if ($request->count < 5) {
-    	 	return response()->json(['msg'=>'sorry you must add at least 5 polygons']);
-    	 }else{
+        $locations = collect(json_decode($request->data, true));
+
+        if ($request->count < 5) {
+            return response()->json(['msg' => 'sorry you must add at least 5 polygons']);
+        } else {
 
 
             $area = Area::create([
-                'name_ar'     => $request->area_ar,
-                'name_en'     => $request->area_en,
-                'city'        => $request->city_id,
-                'status'        => 'active',
+                'name_ar' => $request->area_ar,
+                'name_en' => $request->area_en,
+                'city' => $request->city_id,
+                'status' => 'active',
             ]);
 
-    	 foreach ($locations as $key => $location) {
-    	 	
-    	 	$Polygon = Polygon::create([
-    	 		'lat'     => $location['lat'],
-    	 		'lon' 	  => $location['lng'],
-    	 		'area_id' => $area->id,
-    	 	]);
-                
-    	 }
-    	 	 return response()->json('done');
+            foreach ($locations as $key => $location) {
 
-    	 }
+                $Polygon = Polygon::create([
+                    'lat' => $location['lat'],
+                    'lon' => $location['lng'],
+                    'area_id' => $area->id,
+                ]);
+
+            }
+            return response()->json('done');
+
+        }
     }
 
     public function status()
@@ -80,23 +82,23 @@ class LocationController extends Controller
         $area = Area::find(request()->id);
 
         if ($area) {
-        	
-        	$area->status === 'active' ?  $area->update(['status'=>'deactive']) :  $area->update(['status'=>'active']);
 
-          return redirect()->back()->withStatus(__('status changed successfully'));
+            $area->status === 'active' ? $area->update(['status' => 'deactive']) : $area->update(['status' => 'active']);
 
-        }else{
+            return redirect()->back()->withStatus(__('status changed successfully'));
 
-          return redirect()->back()->withStatus(__('this id not found'));
+        } else {
+
+            return redirect()->back()->withStatus(__('this id not found'));
         }
     }
 
 
     public function showPolygon($id)
     {
-        $polygons = Polygon::Where('area_id',$id)->get();
-    
-        return view($this->blade.'show')->with('polygons', $polygons);
+        $polygons = Polygon::Where('area_id', $id)->get();
+
+        return view($this->blade . 'show')->with('polygons', $polygons);
     }
 
 
@@ -106,13 +108,31 @@ class LocationController extends Controller
         if ($area) {
 
             $area->delete();
-             return redirect()->back()->withStatus(__('delete_successfuly')); 
-        }else{
-           return redirect()->back()->withStatus(__('this Area not found')); 
+            return redirect()->back()->withStatus(__('delete_successfuly'));
+        } else {
+            return redirect()->back()->withStatus(__('this Area not found'));
         }
     }
 
+    public function test()
+    {
+        $polygons = array("-50 30","50 70","100 50","80 10","110 -10","110 -30","-20 -50","-30 -40","10 -10","-10 10","-30 -20","-50 30");
+        $points = array("50 70","70 40","-20 30","100 10","-10 -10","40 -20","110 -20");
+        $data = [];
+        foreach ($polygons as $polygon) {
+            $data[] = new Point(explode(" ",$polygon)[0],explode(" ",$polygon)[1]);
+        }
+        $bounds = new Bounds($data);
+        $point = explode(" ",$points[0]);
+        $is_inside  =[];
+        foreach ($points as $point)
+        {
+            $point = explode(" ",$point);
+        $is_inside[$point[0]] = $bounds->contains(new Point($point[0],$point[1]));
 
-   
+        }
+        return response()->json($is_inside);
+    }
+
 
 }
---
+++ app/Location/Geo/Projection/SphericalMercator.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Location\Geo\Projection;
+
+
+class SphericalMercator
+{
+    public const R = 6378137;
+    public const MAX_LATITUDE =  85.0511287798;
+}
