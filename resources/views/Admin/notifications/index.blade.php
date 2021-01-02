bd502c2aefbd96ba05a543e27550aff34ca9cb19
$   git config --global user.name Your <Ahmedhamdy-eng>
2021-01-02 04:06:09 +0200

    notification

--- resources/views/Admin/notifications/index.blade.php
+++ resources/views/Admin/notifications/index.blade.php
@@ -58,7 +58,7 @@
                                     </thead>
                                     <tbody>
                                     @foreach($notifications as $notification)
-                                    @dd($notification->data["data"]);
+                                    @dd($notification->data );
                                         <tr>
                                             <td>{{__($notification->data["data"],["num"=>$notification->data["id"]])}}</td>
    
