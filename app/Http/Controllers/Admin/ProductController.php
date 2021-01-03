bd199f8b96f9c03aebfc0e7f91d42c6131637d67
Ahmed Hamdy <a.hamdy@eramint.net>
2021-01-03 12:51:55 +0200

    asds

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -218,7 +218,7 @@ class ProductController extends Controller
                 'vendor_id' => $vendor,
                 'supermarket_id' => $supermarket,
                 //'subcategory_id' => $subcategory,
-                'images' => $product_images,
+                'images' => $images,
                 'barcode' => $barcode,
                 'arab_description' => $arab_description,
                 'eng_description' => $eng_description,
