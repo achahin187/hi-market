bc734e150aeda8a0a14b166bdb6c955e95b0cfd9
Mostafa Hesham <motaheshamabdel@gmail.com>
2021-01-04 19:40:54 +0200

    updated location classes

--- app/Location/Geo/Crs/Earth.php
+++ app/Location/Geo/Crs/Earth.php
@@ -14,13 +14,13 @@ class Earth extends CRS
     public $wrapLang = [-180, 180];
     public $R = 6371000;
 
-    public function distance(LatLng $latlng1, LatLng $latLng2)
+    public function distance(LatLng $latLng1, LatLng $latLng2)
     {
         $rad = pi() / 180;
-        $lat1 = $latlng1->lat * $rad;
+        $lat1 = $latLng1->lat * $rad;
         $lat2 = $latLng2->lat * $rad;
-        $sinDLat = sin(($latLng2->lat - $latlng1->lat) * $rad / 2);
-        $sinDLon = sin(($latLng2->lng - $latlng1->lng) * $rad / 2);
+        $sinDLat = sin(($latLng2->lat - $latLng1->lat) * $rad / 2);
+        $sinDLon = sin(($latLng2->lng - $latLng1->lng) * $rad / 2);
         $a = $sinDLat * $sinDLat + cos($lat1) * cos($lat2) * $sinDLon * $sinDLon;
         $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
         return $this->R * $c;
