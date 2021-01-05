d589550b44c76d339eca787f9f97ebb4d7090046
Ahmed Hamdy <a.hamdy@eramint.net>
2021-01-05 19:20:49 +0200

    sagd

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -140,7 +140,7 @@ class ProductController extends Controller
 
                 ]);
               }
-                 dd($notTopic->pluck('topic')->->unique('topic'));
+                 dd($notTopic->pluck('topic')->unique('topic'));
                 return $this->returnData(["supermarkets", "offers","isOffer", "totalMoney", 'topics', 'nonTopic'], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),$this->getOffer()->total_order_money??0,$getPolygon->topic, $notTopic->pluck('topic')->unique('topic')]);
             }//end if 
 
