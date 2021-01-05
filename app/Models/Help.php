901e62eec45aac87dba0fdcc20c5466d903742b8
Ahmed Hamdy <a.hamdy@eramint.net>
2021-01-05 18:06:07 +0200

    adaf

--- app/Models/Help.php
+++ app/Models/Help.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Help extends Model
 {
     protected $guarded = [];
+
+    public function getTitleAttribute()
+    {
+    	return app()->getLocale() == "en" ? $this->title_en : $this->title_ar;
+    }
+
+    protected function getDescriptionAttribute()
+    {
+        return app()->getLocale() == "en" ? $this->description_en : $this->description_ar;
+    }
+
 }
