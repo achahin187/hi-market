846f9a203380275503bda6c6c77513860047d6df
Ahmed Hamdy <a.hamdy@eramint.net>
2021-01-06 11:17:31 +0200

    total money home

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -112,7 +112,7 @@ class ProductController extends Controller
                       ]);
                   
                      
-                      return $this->returnData(["supermarkets", "offers","isOffer","totalMoney", 'topics', 'nonTopic'], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),$this->getOffer()->total_order_money??0, $getPolygon->topic, $notTopic->unique('topic')->pluck('topic')]);
+                      return $this->returnData(["supermarkets", "offers","isOffer","totalMoney", 'topics', 'nonTopic'], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),(string)$this->getOffer()->total_order_money??"0", $getPolygon->topic, $notTopic->unique('topic')->pluck('topic')]);
 
 
                   } else {
@@ -133,7 +133,7 @@ class ProductController extends Controller
 
                 ]);
               }else{
-                
+
                 Udid::create([
                     "body" => $request->header("udid"),
                     'lat' => $request->lat,
@@ -142,7 +142,7 @@ class ProductController extends Controller
                 ]);
               }
 
-                return $this->returnData(["supermarkets", "offers","isOffer", "totalMoney", 'topics', 'nonTopic'], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),$this->getOffer()->total_order_money??0,$getPolygon->topic, $notTopic->unique('topic')->pluck('topic')]);
+                return $this->returnData(["supermarkets", "offers","isOffer", "totalMoney", 'topics', 'nonTopic'], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),(string)$this->getOffer()->total_order_money??"0",$getPolygon->topic, $notTopic->unique('topic')->pluck('topic')]);
             }//end if 
 
         }else{//else data
@@ -154,7 +154,7 @@ class ProductController extends Controller
 
                 if ($client) {
 
-                    return $this->returnData(["supermarkets", "offers","isOffer","totalMoney"], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),$this->getOffer()->total_order_money??0]);
+                    return $this->returnData(["supermarkets", "offers","isOffer","totalMoney"], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),(string)$this->getOffer()->total_order_money??"0"]);
 
 
                 } else {
@@ -168,7 +168,7 @@ class ProductController extends Controller
 
                 ]);
 
-                return $this->returnData(["supermarkets", "offers","isOffer", "totalMoney"], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),$this->getOffer()->total_order_money??0]);
+                return $this->returnData(["supermarkets", "offers","isOffer", "totalMoney"], [HomeDataResource::collection($supermarkets), OfferResource::collection($offers),!!$this->getOffer(),(string)$this->getOffer()->total_order_money??"0"]);
             }//end if 
         }//end data
     }
--- app/Http/Resources/HomeDataResource.php
+++ app/Http/Resources/HomeDataResource.php
@@ -29,8 +29,8 @@ class HomeDataResource extends JsonResource
                 "city"=> $this->city->name?? '',
                 "imagepath"=> asset('branche_image/'.$this->image),
                 "logopath"=>asset('branche_image/'.$this->logo),
-                'isOffer'=> !!$this->getOffer(),
-                'totalMoney'=> (string)$this->getOffer() != null ? (string)$this->getOffer()->total_order_money :"0",
+                // 'isOffer'=> !!$this->getOffer(),
+                // 'totalMoney'=> (string)$this->getOffer() != null ? (string)$this->getOffer()->total_order_money :"0",
                 
 
             ];
