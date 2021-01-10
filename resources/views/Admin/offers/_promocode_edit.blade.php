41bc267a431d15452bf47e3d8977911dd993fc42
Ahmed Hamdy <a.hamdy@eramint.net>
2021-01-10 15:45:59 +0200

    safa

--- resources/views/Admin/offers/_promocode_edit.blade.php
+++ resources/views/Admin/offers/_promocode_edit.blade.php
@@ -23,7 +23,7 @@
                         <label for="branch">SuperMarket</label>
                         <select name="supermarket_id" id='supermarket_4'  class="form-control select2">
                         
-                            <option  selected  disabled>Please Select Source</option>
+                            <option    disabled>Please Select Source</option>
                         @foreach( $supermarkets as  $supermarket) 
                             <option  @if(old("supermarket_id") == $supermarket->id) selected
                                         @endif value="{{$supermarket->id}}"{{ $offer->supermarket_id ==  $supermarket->id ? 'selected' : ''}}>{{$supermarket->name}}</option>
