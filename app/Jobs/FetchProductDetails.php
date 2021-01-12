a971cdc93f57af86bdd1d5d8c50875010d05ce90
Mostafa Hesham <motaheshamabdel@gmail.com>
2021-01-12 23:04:09 +0200

    fixed a bug

--- app/Jobs/FetchProductDetails.php
+++ app/Jobs/FetchProductDetails.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Response\Response;
 use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -17,18 +18,24 @@ class FetchProductDetails implements ShouldQueue
     private $client;
     private $data;
     private $lang;
+    private $category;
+    private $is_subcategory;
 
     /**
      * Create a new job instance.
      *
      * @param $product
      * @param $lang
+     * @param $category
+     * @param $is_subcategory
      */
-    public function __construct($product, $lang)
+    public function __construct($product, $lang, $category, $is_subcategory)
     {
 
         $this->product = $product;
         $this->lang = $lang;
+        $this->category = $category;
+        $this->is_subcategory = $is_subcategory;
     }
 
     /**
@@ -49,22 +56,52 @@ class FetchProductDetails implements ShouldQueue
 
         $response = $this->client->post("https://prod.thegroceryshop.com/web_services14/product_detail", ["body" => json_encode($body)]);
         $product = json_decode($response->getBody())->response->data->Product;
+
         dump($product->english_product_name);
         if (!file_exists(public_path("data/productdetails"))) {
             mkdir(public_path("data/productdetails"));
         }
-        foreach ($product->File as $index => $image) {
+        $productResponse = new Response();
+        $productResponse->category_id = $this->category->category_id;
+        $productResponse->in_wishlist = $product->in_wishlist;
+        $productResponse->product_average_rating = $product->product_rating_count;
+        $productResponse->product_id = $product->product_id;
+        $productResponse->brand_name = $product->brand_name;
+        $productResponse->arabic_brand_name = $product->arabic_brand_name;
+        $productResponse->english_brand_name = $product->english_brand_name;
+        $productResponse->brand_id = $product->brand_id;
+        $productResponse->door_step = $product->door_step;
+        $productResponse->shipping_hours = $product->shipping_hours;
+        $productResponse->is_subcategory = (int)$this->is_subcategory;
+        $productResponse->product_acutal_price = $product->product_acutal_price;
 
-            $fileName = time() . uniqid() . ".jpg";
-            if ($this->lang != 2) {
-                $this->client->get($image->image, ["sink" => fopen(public_path("data/productdetails/" . (Str::snake($fileName))), "w")]);
-            }
+        $productResponse->product_offer_price = $product->product_offer_price;
+        $productResponse->prodcut_offer_percent = $product->product_offer_percent;
+        $productResponse->offered_price_status = $product->offered_price_status;
+        $productResponse->offered_start_date = $product->offered_start_date;
+        $productResponse->offered_end_date = $product->offered_end_date;
+        $productResponse->loyalty_points = $product->loyalty_points;
 
+        $productResponse->delay_shipment = $product->delay_shipment;
+        $productResponse->reorder_point = $product->reorder_point;
+        $productResponse->mark_sold_qty = $product->mark_sold_qty;
+        $productResponse->product_name = $product->product_name;
+        $productResponse->arabic_product_name = $product->arabic_product_name;
+        $productResponse->english_product_name = $product->english_product_name;
+        $productResponse->english_product_size = $product->english_product_size;
+        $productResponse->arabic_product_size = $product->arabic_product_size;
+        $productResponse->currency = $product->currency;
+        $productResponse->product_size = $product->product_size;
+        $productResponse->File = $product->File;
+        $productResponse->FileBig = $product->FileBig;
+        $productResponse->shipping_third_party = $product->shipping_third_party;
+        $productResponse->product_rating = $product->product_rating;
+        $productResponse->saving = $product->saving;
+        $productResponse->product_description = $product->product_description;
+        $productResponse->qty = $product->qty;
+        $productResponse->tax = $product->tax;
 
-            $product->File[$index]->image = $fileName;
-            dump($fileName);
-        }
-        $this->data->add($product);
+        $this->data->add($productResponse->getAttributes());
         $path = public_path("products/" . ($this->lang == 1 ? "en" : "ar"));
         if (!file_exists($path)) {
             mkdir($path);
