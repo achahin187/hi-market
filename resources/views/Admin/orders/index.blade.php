92f53bd61068b3322bd8a8eacd8fb9c4713e0fb8
Ahmed Hamdy <a.hamdy@eramint.net>
2021-01-12 17:56:14 +0200

    sadf

--- resources/views/Admin/orders/index.blade.php
+++ resources/views/Admin/orders/index.blade.php
@@ -204,11 +204,11 @@
 
         @if(auth()->user()->can('order-previous'))
 
-        <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'previous','order_id'=>$order->id]) }}" class="btn btn-success {{ $order->status == 0 ? 'disabled' : '' }}">Previous</a></td>
+        <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'previous','order_id'=>$order->id]) }}" class="btn btn-success {{ in_array($order->status, [0,6])  ? 'disabled' : '' }}">Previous</a></td>
         @endif
 
         @if(auth()->user()->can('order-next'))
-        <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'next','order_id'=>$order->id]) }}" class="btn btn-primary {{ $order->status >= 4 ? 'disabled' : '' }}" >Next</a></td>
+        <td><a href="{{ route('order.change.status',['order_status'=>$order->status, 'type'=>'next','order_id'=>$order->id]) }}" class="btn btn-primary {{ in_array($order->status, [4,6]) ? 'disabled' : '' }}" >Next</a></td>
         @endif
 
      
